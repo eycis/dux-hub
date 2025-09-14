ceae27cd30a755209025fafd3938a3bf8fc7fb59
eycis <eycisxxx@gmail.com>
2025-09-14 23:07:09 +0200

    routing, header

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        pathMatch: "full",
+        loadComponent : () => {
+            return import("./home/home.component").then((m)=> m.HomeComponent);
+        },
+    },
+    {
+        path: "experiments",
+        pathMatch: "full",
+        loadComponent : () => {
+            return import("./home/home.component").then((m)=> m.HomeComponent);
+        },
+    },
+    {
+        path: "feedback",
+        pathMatch: "full",
+        loadComponent : () => {
+            return import("./home/home.component").then((m)=> m.HomeComponent);
+        },
+    },
+    {
+        path: "reports",
+        pathMatch: "full",
+        loadComponent : () => {
+            return import("./home/home.component").then((m)=> m.HomeComponent);
+        },
+    },
+    {
+        path: "integration",
+        pathMatch: "full",
+        loadComponent : () => {
+            return import("./home/home.component").then((m)=> m.HomeComponent);
+        },
+    },
+];
