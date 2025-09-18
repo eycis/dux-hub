2f676e06a3bf8b403fa04c57ddabb571c45382b8
eycis <eycisxxx@gmail.com>
2025-09-18 23:15:02 +0200

    postgres prep

--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -3,5 +3,9 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     API_PREFIX: str = "/api"
     CORS_ORIGINS: list[str] = ["http://localhost:4200"]
+    DATABASE_URL: str
 
+    class Config:
+        env_file= ".env"
+        
 settings = Settings()
